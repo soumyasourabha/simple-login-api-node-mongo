8166ba73433d436c0badc2fac98842ccb1b29cb6
soumyasourabha <53940285+soumyasourabha@users.noreply.github.com>
2021-01-27 09:08:39 +0530

    adding audit role in backend logic

---
+++ config.js
@@ -0,0 +1,6 @@
+const str = {
+  "connectionString": "mongodb+srv://Byteridge:byteridge@shopspree-dev-env.qw5jz.mongodb.net/interview?retryWrites=true&w=majority",
+  "secret": "JBJYBJ23jkj232kALKMLAK"
+}
+
+module.exports = str;
